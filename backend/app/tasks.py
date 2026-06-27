7943f3c407de0efc67131baa7631c2fbae361399
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 11:47:36 -0700

    Refactor to use correct syntax for demucs version

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,25 +1,42 @@
 # the stem separator — the "work"
 from pathlib import Path
-from demucs.api import Separator, save_audio
+from demucs.pretrained import get_model
+from demucs.apply import apply_model
+from demucs.audio import AudioFile, save_audio
+import torch
 
-_separator = None
+model = None
 
 
-def get_separator():
-    global _separator
-    if _separator is None:
-        _separator = Separator(model="htdemucs_6s", device="mps")
-    return _separator
+def get_model():
+    global model
+    if model is None:
+        model = get_model("htdemucs_6s")
+        model.to("mps")
+        model.eval()
+    return model
 
 
 def stem_separator(input_path: str, output_directory: str):
     """Creates a new file for each instrument"""
-    separator = get_separator()
-    origin, stems = separator.separate_audio_file(input_path)
+    wav = AudioFile(input_path).read(
+        streams=0, samplerate=model.samplerate, channels=model.audio_channels
+    )
+    ref = wav.mean(0)
+    wav = (wav - ref.mean()) / ref.std()  # demucs normalizes by the mix's mean/std
 
-    for name, tensor in stems:
+    with torch.no_grad():
+        sources = apply_model(model, wav[None], device="mps")[
+            0
+        ]  # wav[None] adds a batch dim
+
+    sources = sources * ref.std() + ref.mean()  # un-normalize
+
+    for name, source in zip(
+        model.sources, sources
+    ):  # model.sources == the 6 stem names
         save_audio(
-            tensor,
-            (Path(output_directory) / f"{name}.wav"),
-            samplerate=separator.samplerate,
+            source,
+            str(Path(output_directory) / f"{name}.wav"),
+            samplerate=model.samplerate,
         )
