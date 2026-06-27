f1ea0afd9fda98d09fff10e4fdac0169ea37d88e
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:56:02 -0700

    Create JobStatus class to handle types with FastAPI

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,17 +1,26 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, HTTPException
 from rq.job import Job
 from rq.exceptions import NoSuchJobError
+from pydantic import BaseModel
 
 from app.queue import redis_client
 
 router = APIRouter(prefix="/jobs")
 
 
+class JobStatus(BaseModel):
+    id: str
+    status: str
+
+
 @router.get("/{job_id}")
-def get_job_by_id(job_id: str) -> Job:
+def get_job_by_id(job_id: str) -> JobStatus:
     try:
         job = Job.fetch(job_id, redis_client)
-        return job
+        return JobStatus(id=job_id, status=job.get_status())
 
     except NoSuchJobError:
-        print(f"Job {job_id} does not exist or has expired in Redis.")
+        raise HTTPException(
+            status_code=404,
+            details=f"Job {job_id} does not exist or has expired in Redis.",
+        )
