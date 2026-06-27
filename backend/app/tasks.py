39b36ec3477c1e9959d1bfeb309920c5cac16119
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:10:20 -0700

    Use context manager for file creation

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -2,4 +2,5 @@
 def stem_separator(input_path: str, output_directory: str):
     """Creates a new file for each instrument"""
     for instrument in ["drums", "bass", "vocals", "other", "guitar", "piano"]:
-        open(f"{output_directory}/{instrument}_stem", "x")
+        with open(f"{output_directory}/{instrument}_stem", "w") as f:
+            f.write("Initial file setup complete.")
