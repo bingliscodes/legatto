7ff2e7e466fe2b3810eb708a35d5bb614d744304
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:01:06 -0700

    Fix getenv syntax

--- backend/app/queue.py
+++ backend/app/queue.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Create Redis connection
-url = os.environ["REDIS_URL"]
+url = os.getenv("REDIS_URL")
 redis_client = Redis.from_url(url, decode_response=True)
 task_queue = Queue(connection=redis_client)
