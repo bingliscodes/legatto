79e4bddcd7dadce29ad3a521ff2fc9bcee1ab914
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:46:36 -0700

    Add parenthesis to ensure correct operation order

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -6,4 +6,4 @@ def stem_separator(input_path: str, output_directory: str):
     """Creates a new file for each instrument"""
 
     for instrument in ["drums", "bass", "vocals", "other", "guitar", "piano"]:
-        Path(output_directory) / f"{instrument}_stem".write_text("placeholder")
+        (Path(output_directory) / f"{instrument}_stem").write_text("placeholder")
