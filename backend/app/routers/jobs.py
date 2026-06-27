492858db24ed8a103402d4260bf2fd3cbbee4716
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 09:49:20 -0700

    Add job fetcher

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,6 +1,14 @@
-from fastapi import UploadFile, APIRouter, status, Response
+from fastapi import APIRouter, status, Response
+from rq.job import Job
+from app.queue import redis_conn
 
 router = APIRouter(prefix="/jobs")
 
 
-# GET /jobs{job_id}
+@router.get("/{job_id}")
+def get_job_by_id(job_id: str) -> Job | None:
+    job = Job.fetch(job_id, redis_conn)
+    if not job:
+        return None
+
+    return job
