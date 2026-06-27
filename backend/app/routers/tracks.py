b27f646251e4c5e10cd1e4dd6800e5f85bf5a9d1
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 09:44:03 -0700

    Add process_audio function to save file and enqueue stem splitter

--- backend/app/routers/tracks.py
+++ backend/app/routers/tracks.py
@@ -1,4 +1,30 @@
 from fastapi import UploadFile, APIRouter, status, Response
+from pathlib import Path
+
+from app.queue import redis_conn, task_queue
+from app.tasks import stem_separator
 
 router = APIRouter(prefix="/tracks")
 # POST /tracks
+
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+
+@router.post("/")
+async def proccess_audio(audio_file: UploadFile):
+    """Takes in an audio file, save to disk, drop the job in the queue, return job id"""
+    await save_file_to_disk(audio_file)
+    job = task_queue.enqueue(stem_separator)
+    return job.id
+
+
+async def save_file_to_disk(file: UploadFile):
+    contents = await file.read()
+
+    destination_path = UPLOAD_DIR / file.filename
+
+    with open(destination_path, "wb") as f:
+        f.write(contents)
+
+    return {"message": f"Successfully saved to {destination_path}"}
