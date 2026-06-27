8a9cc944d2ec5df2bc7a28520e6d22fde065396b
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 09:23:58 -0700

    Move stem_separator to tasks module

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -3,10 +3,4 @@ from fastapi import UploadFile, APIRouter, status, Response
 router = APIRouter(prefix="/jobs")
 
 
-def stem_separator(input_path: str, output_directory: str):
-    """Creates a new file for each instrument"""
-    for instrument in ["drums, bass, vocals, other, guitar, piano"]:
-        open(f"{output_directory}/{instrument}_stem", "x")
-
-
 # GET /jobs{job_id}
