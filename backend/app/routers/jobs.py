3f28dad3571bad3881a9ffd284822f26919235ad
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 12:39:45 -0700

    Extract stems and locations into dict to return from job fetcher

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -3,6 +3,9 @@ from rq.job import Job
 from rq.exceptions import NoSuchJobError
 from pydantic import BaseModel
 
+from collections import defaultdict
+
+from app.config import STORAGE_ROOT
 from app.queue import redis_client
 
 router = APIRouter(prefix="/jobs")
@@ -18,7 +21,13 @@ class JobStatus(BaseModel):
 def get_job_by_id(job_id: str) -> JobStatus:
     try:
         job = Job.fetch(job_id, redis_client)
-        return JobStatus(id=job.id, status=job.get_status())
+        stems_dir = STORAGE_ROOT / job_id / "stems"
+        stems = {
+            file.stem: f"/tracks/{job_id}/stems/{file.name}"
+            for file in stems_dir.glob("*.wav")
+        }
+
+        return JobStatus(id=job.id, status=job.get_status(), stems=stems)
 
     except NoSuchJobError:
         raise HTTPException(
