f5f9daf3adb0722d71ccad270fe26337647f048d
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:57:06 -0700

    take job meta data from fetched job

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Response, HTTPException
+from fastapi import APIRouter, HTTPException
 from rq.job import Job
 from rq.exceptions import NoSuchJobError
 from pydantic import BaseModel
@@ -17,7 +17,7 @@ class JobStatus(BaseModel):
 def get_job_by_id(job_id: str) -> JobStatus:
     try:
         job = Job.fetch(job_id, redis_client)
-        return JobStatus(id=job_id, status=job.get_status())
+        return JobStatus(id=job.id, status=job.get_status())
 
     except NoSuchJobError:
         raise HTTPException(
