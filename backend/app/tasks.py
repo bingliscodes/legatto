52cca27ae89708ecd6c609ec4556724d291222c0
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 11:40:51 -0700

    Use lazy singleton to load large demucs separator

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -2,10 +2,19 @@
 from pathlib import Path
 from demucs.api import Separator, save_audio
 
+_separator = None
+
+
+def get_separator():
+    global _separator
+    if _separator is None:
+        _separator = Separator(model="htdemucs_6s", device="mps")
+    return _separator
+
 
 def stem_separator(input_path: str, output_directory: str):
     """Creates a new file for each instrument"""
-    separator = Separator(model="htdemucs_6s", device="mps")
+    separator = get_separator()
     origin, stems = separator.separate_audio_file(input_path)
 
     for name, tensor in stems:
