2f8c9f3c640bbf8e90648277358267cf09f48b13
Benjamin Inglis <bjammin134@gmail.com>
2026-06-27 10:12:26 -0700

    Handle NoSuchJobError on job fetcher

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, status, Response
 from rq.job import Job
+from rq.exceptions import NoSuchJobError
 from app.queue import redis_client
 
 router = APIRouter(prefix="/jobs")
 
 
 @router.get("/{job_id}")
-def get_job_by_id(job_id: str) -> Job | None:
-    job = Job.fetch(job_id, redis_client)
-    if not job:
-        return None
+def get_job_by_id(job_id: str) -> Job:
+    try:
+        job = Job.fetch(job_id, redis_client)
+        return job
 
-    return job
+    except NoSuchJobError:
+        print(f"Job {job_id} does not exist or has expired in Redis.")
