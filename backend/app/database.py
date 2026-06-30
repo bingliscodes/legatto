7b1dae91b9259230aecdd7fbdec1f4e95680bca8
Benjamin Inglis <bjammin134@gmail.com>
2026-06-30 13:43:01 -0700

    Fix typos in db_url string

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db_url = f"postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_USER"]}@localhost:5432/{os.environ["DB_NAME"]}"
+db_url = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@localhost:5432/{os.environ['DB_NAME']}"
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
