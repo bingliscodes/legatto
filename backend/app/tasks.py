372dcdceb2df92bd3ed3ddf55824a93743574d2b
Benjamin Inglis <bjammin134@gmail.com>
2026-06-30 14:42:18 -0700

    First pass at stem_separator integrated with DB

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -5,11 +5,9 @@ from demucs.pretrained import get_model
 from demucs.apply import apply_model
 from demucs.audio import AudioFile, save_audio
 import torch
+import uuid
 
-
-from app.config import settings
-
-
+from app.models.track import Track, TrackStatus
 from app.config import SHIFTS, OVERLAP
 
 model = None
@@ -26,28 +24,40 @@ def get_demucs_model():
 
 def stem_separator(track_id: str, input_path: str, output_directory: str):
     """Creates a new file for each instrument"""
-    db = SessionLocal()
     model = get_demucs_model()
-    wav = AudioFile(input_path).read(
-        streams=0, samplerate=model.samplerate, channels=model.audio_channels
-    )
-    ref = wav.mean(0)
-    wav = (wav - ref.mean()) / ref.std()  # demucs normalizes by the mix's mean/std
+    db = SessionLocal()
+    try:
+        job = db.get(Track, uuid.UUID(track_id))
+        job.status = TrackStatus.processing
+        db.commit()
 
-    with torch.no_grad():
-        sources = apply_model(
-            model, wav[None], device="mps", shifts=SHIFTS, overlap=OVERLAP
-        )[
-            0
-        ]  # wav[None] adds a batch dim
-
-    sources = sources * ref.std() + ref.mean()  # un-normalize
-
-    for name, source in zip(
-        model.sources, sources
-    ):  # model.sources == the 6 stem names
-        save_audio(
-            source,
-            str(Path(output_directory) / f"{name}.wav"),
-            samplerate=model.samplerate,
+        # Processing work
+        wav = AudioFile(input_path).read(
+            streams=0, samplerate=model.samplerate, channels=model.audio_channels
         )
+        ref = wav.mean(0)
+        wav = (wav - ref.mean()) / ref.std()  # demucs normalizes by the mix's mean/std
+
+        with torch.no_grad():
+            sources = apply_model(
+                model, wav[None], device="mps", shifts=SHIFTS, overlap=OVERLAP
+            )[
+                0
+            ]  # wav[None] adds a batch dim
+
+        sources = sources * ref.std() + ref.mean()  # un-normalize
+
+        for name, source in zip(
+            model.sources, sources
+        ):  # model.sources == the 6 stem names
+            save_audio(
+                source,
+                str(Path(output_directory) / f"{name}.wav"),
+                samplerate=model.samplerate,
+            )
+    except:
+        job.status = TrackStatus.failed
+        db.commit()
+        # Re raise?
+    finally:
+        db.close()
