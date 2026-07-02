6214b417490a988fecfe558e059c19777a6ddb97
Benjamin Inglis <bjammin134@gmail.com>
2026-07-02 15:08:56 -0700

    Create test input file for Docker image debugging

---
+++ backend/load.py
@@ -0,0 +1,7 @@
+from app.storage import S3Storage
+
+s = S3Storage()
+audio_path = "/Users/benjamininglis/Code/guitarist-practice-tool/backend/storage/dda607de-493b-443a-8197-509d14b5741b/Glasglow Kiss.mp3"
+with open(audio_path, "rb") as f:
+    s.write_file("test123/song.mp3", f.read())
+print("uploaded ->", "test123/song.mp3")
