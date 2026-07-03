1365fccfa2eb6e3865bc4bc5028e977b54f98215
Benjamin Inglis <bjammin134@gmail.com>
2026-07-03 11:37:38 -0700

    Update vite config to forward requests to relative path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
 });
