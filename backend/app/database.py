3a2a821700dd6816e9e347d39aede51a1349b9c5
Benjamin Inglis <bjammin134@gmail.com>
2026-07-08 11:01:26 -0700

    Run alembic migration to add users table and set FK

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,19 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from app.config import settings
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=convention)
 
 
 engine = create_engine(settings.database_url)
