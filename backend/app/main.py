2ff52e2f893a40b772ee6eace6412d40bcab98b7
Benjamin Inglis <bjammin134@gmail.com>
2026-07-08 11:17:21 -0700

    Add session middleware to app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from .routers import tracks as tracks_router
+from config import settings
 
 app = FastAPI(title="Guitarist Practice Tool API")
 
@@ -14,6 +16,14 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.session_secret,
+    same_site="lax",
+    https_only=settings.session_https_only,
+    max_age=60 * 60 * 24 * 365,  # ~1yr
 )
 
 app.include_router(tracks_router.router, prefix="/api")
