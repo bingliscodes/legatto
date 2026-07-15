f60a03515ed40333fdfc4a96c0de56c96e243a42
Benjamin Inglis <bjammin134@gmail.com>
2026-07-15 12:51:47 -0700

    First pass at implementing DAU count with redis

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,12 +1,19 @@
 import uuid
 from fastapi import Request, Depends
 from sqlalchemy.orm import Session
+from redis import Redis
+from datetime import datetime, timezone
+
+from app.config import settings
 from app.database import get_db
 from app.models.user import User
+
+redis_client = Redis.from_url(settings.redis_url, decode_responses=True)
 
 
 def get_current_user_id(request: Request, db: Session = Depends(get_db)) -> uuid.UUID:
     session_id = request.session.get("user_id")
+    redis_client.sadd(f"active:{datetime.now(timezone.utc).date()}", str(session_id))
 
     if session_id:
         return uuid.UUID(session_id)
